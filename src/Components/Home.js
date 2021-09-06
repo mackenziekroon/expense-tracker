827283385a63f8fcc7fe8229cac827d207fa528d
Mackenzie Kroon <kroonmackenzie@gmail.com>
2021-09-06 16:36:44 -0500

    home screen css

---
+++ src/Components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = (props) => {
+  return (
+    <div className="global-screen">
+      <h1 className="home-text">Expense </h1>
+      <h1 className="home-text"> Tracker</h1>
+      <h2 className="get-started-btn">
+        <div className="get-started">Get Started!</div>
+      </h2>
+    </div>
+  );
+};
+
+export default Home;
